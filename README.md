7963ffe108db905ce831a3d132963faf284add5c
TacosTea <tacos@tacostea.net>
2017-06-21 16:03:56 +0900

    並行処理で速度改善 & 対象インスタンスの限定を可能にした

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # versionbattle
 鯖缶御用達の新バージョン追従バトルを観測するやつ
+
+## つかいかた
+1. とりあえず使いたい
+```bash
+$ ./version.sh
+<バージョン番号> <インスタンス名>
+<バージョン番号> <インスタンス名>
+<バージョン番号> <インスタンス名>
+.
+.
+.
+```
+パイプしてsortするもよし、保存してムフフするもよし。
+
+2. おなじみのインスタンスとversionbattleしたい
+まず対象インスタンス名の書かれたinstances.listを用意します。
+とりあえずinstances.mastodon.xyzのものを使いたい場合はこれで。
+```bash
+curl -s https://instances.mastodon.xyz/instances.json | jq -r '.[].name' > instances.list
+```
+instances.listが用意できたらこれ。
+```bash
+$ ./version.sh
+```
+実行するコマンドは同じでも、結果はresults.txtに残る。(標準出力されない)
+
--- version.sh
+++ version.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-# Get instance name list and save
-INSTANCELIST=$(curl -s https://instances.mastodon.xyz/instances.json | jq -r '.[].name')
-
-for INSTANCE in $INSTANCELIST; do
-  LINK="https://$INSTANCE/about/more"
-  VER=`curl -s $LINK | grep -E "<strong>[0-9]\.[0-9]\.[0-9]</strong>" | sed -r 's/.*>([0-9\.]+).*/\1/'`
+function get_version() {
+  DOMAIN=$1
+  LINK="https://$DOMAIN/about/more"
+  VER_LAW=$(curl -m 10 -s $LINK | grep -E "<strong>[0-9]\.[0-9]\.[0-9]</strong>" $PAGE)
+  VER=$(echo $VER_LAW | sed -r 's/.*>([0-9\.]+).*/\1/')
   if [ -n "$VER" ]; then
-    echo "$VER $INSTANCE"
+    echo "$VER $DOMAIN"
   fi
-done
+}
+
+export -f get_version
+
+if [ -f instances.list ]; then
+  xargs -n1 -P0 -I % bash -c "get_version $INSTANCE %" < instances.list > results.list
+else
+  curl -s https://instances.mastodon.xyz/instances.json | jq -r '.[].name' > .instances.list
+  xargs -n1 -P0 -I % bash -c "get_version $INSTANCE %" < .instances.list
+  rm -f .instances.list
+fi
