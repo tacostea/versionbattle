7603acc88ef9d8cc365d1d21f820a1c9078241f9
ikuradon <hawaiianphoto@geekhost.net>
2017-06-24 01:27:36 +0900

    スクレイピングからAPIアクセスに変更

--- version.sh
+++ version.sh
@@ -2,9 +2,9 @@
 
 function get_version() {
   DOMAIN=$1
-  LINK="https://$DOMAIN/about/more"
-  VER_LAW=$(curl -m 10 -s $LINK | grep -E "<strong>[0-9]\.[0-9]\.[0-9]</strong>" $PAGE)
-  VER=$(echo $VER_LAW | sed -r 's/.*>([0-9\.]+).*/\1/')
+  LINK="https://$DOMAIN/api/v1/instance"
+  VER_LAW=$(curl -m 10 -s $LINK | jq -r '.version')
+  VER=$(echo $VER_LAW | sed -r 's/.*>([0-9\.]+).*/\1/' | cut -c-5)
   if [ -n "$VER" ]; then
     echo "$VER $DOMAIN"
   fi
