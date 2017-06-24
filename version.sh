2d7c19d4d0216e242238428274f8adc5e1e5eda9
TacosTea <tacos@tacostea.net>
2017-06-24 18:02:48 +0900

    時刻出力、フォーマット固定等、運用を考慮した変更

--- version.sh
+++ version.sh
@@ -2,18 +2,23 @@
 
 function get_version() {
   DOMAIN=$1
-  LINK="https://$DOMAIN/about/more"
-  VER_LAW=$(curl -m 10 -s $LINK | grep -E "<strong>[0-9]\.[0-9]\.[0-9]</strong>" $PAGE)
-  VER=$(echo $VER_LAW | sed -r 's/.*>([0-9\.]+).*/\1/')
-  if [ -n "$VER" ]; then
-    echo "$VER $DOMAIN"
+  LINK="https://$DOMAIN/api/v1/instance"
+  VER_RAW=$(curl -m 30 -s $LINK | jq -r '.version' 2>/dev/null)
+  VER=$(echo $VER_RAW | sed -r 's/.*>([0-9\.]+).*/\1/' | cut -c-5 2>/dev/null)
+  if [[ ! "$VER" =~ [0-9]+(\.[0-9]+){2} ]]; then
+    echo "$DOMAIN ?.?.?"
+  else
+    echo "$DOMAIN $VER"
   fi
 }
 
 export -f get_version
 
 if [ -f instances.list ]; then
-  xargs -n1 -P0 -I % bash -c "get_version $INSTANCE %" < instances.list > results.list
+  mv results.list results.list.old
+  xargs -n1 -P0 -I % bash -c "get_version $INSTANCE %" < instances.list >> results.list
+  sort -u results.list -o results.list
+  sed -i "1s/^/$(LANG=C date)\n/" results.list
 else
   curl -s https://instances.mastodon.xyz/instances.json | jq -r '.[].name' > .instances.list
   xargs -n1 -P0 -I % bash -c "get_version $INSTANCE %" < .instances.list
