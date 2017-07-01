e8d19d9dbc7867a13381a975343f72ab2b0e43f9
TacosTea <tacos@tacostea.net>
2017-07-02 00:13:52 +0900

    /api/v1/instanceのHTTPレスポンスを取得できるようにした

--- version.sh
+++ version.sh
@@ -3,6 +3,7 @@
 function get_version() {
   DOMAIN=$1
   LINK="https://$DOMAIN/api/v1/instance"
+  STATUS=$(curl -m 5 -kL $LINK -w "%{http_code}" 2>/dev/null)
   VER_RAW=$(curl -m 5 -s $LINK | jq -r '.version' 2>/dev/null)
   VER=$(echo $VER_RAW | sed -r 's/.*>([0-9\.]+).*/\1/' | cut -c-5 2>/dev/null)
   if [[ ! "$VER" =~ [0-9]+(\.[0-9]+){2} ]]; then
@@ -21,7 +22,7 @@ if [ -f instances.list ]; then
   sed -i "1s/^/$(LANG=C date)\n/" results.list
 else
   curl -s https://instances.mastodon.xyz/instances.json | jq -r '.[].name' > .instances.list
-  xargs -n1 -P5 -I % bash -c "get_version $INSTANCE %" < .instances.list
+  xargs -n1 -P10 -I % bash -c "get_version $INSTANCE %" < .instances.list
   rm -f .instances.list
 fi
 
