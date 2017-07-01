5b8b9b2fcedb1faa20fb713281a2e789eeeddd03
TacosTea <tacos@tacostea.net>
2017-07-02 02:44:42 +0900

    API/instanceの取得時HTTPレスポンスをDBに反映

--- ping.sh
+++ ping.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-echo "" > time.txt
-echo "" > http_error.txt
-echo "" > ping_error.txt
+echo -n > time.txt
+echo -n > http_error.txt
 
 for DOMAIN in $(cat instances.list); do
-  RESULT=$(curl -m 5 -kL https://$DOMAIN/api/v1/instance -o /dev/null -w "%{time_total} %{http_code}" 2> /dev/null)
+  RESULT=$(curl -m 10 -kL https://$DOMAIN/api/v1/instance -o /dev/null -w "%{time_total} %{http_code}" 2> /dev/null)
 
   TIME=$(echo $RESULT | sed -r 's/([0-9]+\.[0-9]+) ([0-9]{1,3})/\1/')
   STATUS=$(echo $RESULT | sed -r 's/([0-9]+\.[0-9]+) ([0-9]{1,3})/\2/')
     
   if [ "$STATUS" == "200" ]; then
     # HTTP 202 OK の場合は time.txt に追加
+    sudo -u postgres psql -U postgres -d instances -c "UPDATE list SET status = TRUE WHERE uri = '$DOMAIN'" 1>/dev/null 2>/dev/null
     echo "$DOMAIN, $TIME" >> time.txt
   else
-    # それ以外の場合はレスポンスコードを http_error.txt に追加
+    # それ以外の場合はレスポンスコードを http_error.txt に追加してDBに反映
+    # ちなみに 000 => タイムアウト?
+    sudo -u postgres psql -U postgres -d instances -c "UPDATE list SET delay = NULL, status = FALSE WHERE uri = '$DOMAIN'" 1>/dev/null 2>/dev/null
     echo "$DOMAIN, $STATUS" >> http_error.txt
   fi
 done
--- progress.sh
+++ progress.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 P=$(echo $(wc -l *.txt | tail -n1) $(wc -l instances.list| tr -d [:alpha:][:punct:][:blank:]) | awk '{print $1/$3*100;}')
-while [ $(echo "$P < 95"|bc) -eq 1 ]; do 
+while [ $(echo "$P < 100"|bc) -eq 1 ]; do 
   P=$(echo $(wc -l *.txt | tail -n1) $(wc -l instances.list| tr -d [:alpha:][:punct:][:blank:]) | awk '{printf "%2.2f", $1/$3*100;}')
   echo "$P%"
   wc -l *.txt
-  echo -e "\033[6A\r"
+  echo -e "\033[5A\r"
   sleep 1
 done
 echo -e "\033[3B"
-sh ~/push.sh "done" "curl list done"
+sh ~/push.sh "done" "ping.sh has been terminated." 1>/dev/null 2>/dev/null
+echo
