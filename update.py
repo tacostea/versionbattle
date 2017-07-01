84071068617e1f55ba15194ebc3659d0cd54132a
TacosTea <tacos@tacostea.net>
2017-07-02 00:01:09 +0900

    instances.listにのみ存在するuriのDB追加処理

--- update.py
+++ update.py
@@ -1,15 +1,23 @@
 import re
 import postgresql
+import os.path as path
 
 pattern_version = r"^[0-9]+(\.[0-9]+){2}$"
 
 db = postgresql.open("pq://postgres@localhost/instances")
 
 def get_watching_uri():
-  get_uris = db.prepare("select uri from list")
+  get_list = db.prepare("SELECT uri FROM list")
   with db.xact():
-    for row in get_uris():
+    for row in get_list():
      print(row["uri"])
+
+def get_exsistence(uri):
+  get_list = db.prepare("SELECT count(*) FROM list WHERE uri = $1")
+  with db.xact():
+    for row in get_list( uri ):
+      return row[0]
+
 
 def get_version(uri):
   get_list = db.prepare("SELECT version FROM list WHERE uri = $1")
@@ -22,7 +30,19 @@ def get_version(uri):
     else:
       return None
 
+def insert_uri(uri):
+  insert_list = db.prepare("INSERT INTO list(uri) VALUES($1)")
+  insert_list.first(uri)
+
+def update_status(uri, status):
+  None
+
 def update_version(uri, version):
+
+  if get_exsistence(uri) != 1:
+    insert_uri(uri)
+  # update up status
+
   update_list = db.prepare("UPDATE list SET version = $2, updated = now() where uri = $1")
   insert_updates = db.prepare("INSERT INTO updates VALUES($1, now(), $2)") 
   if get_version(uri) == version or not re.compile(pattern_version).search(version) :
@@ -34,9 +54,19 @@ def update_version(uri, version):
         rows += 1
       for row in insert_updates(uri, version):
         rows += 1
+      return 1
+    # postgresql exception
+    return 0
+
+def update_delay(uri, delay):
+  update_list = db.prepare("UPDATE list SET delay = $2 where uri = $1")
+  with db.xact():
+    rows = 0
+    for row in update_list(uri, delay):
+      rows += 1
     return 1
-#      if rows != 4:
-#        raise Exception("Update Error")
+  # postgresql exception
+  return 0
 
 def parse_str(obj):
   if obj is None:
@@ -44,22 +74,43 @@ def parse_str(obj):
   else:
     return str(obj)
 
+def divide_line(line, pattern):
+  if re.compile(pattern).search(line):
+    return re.compile(pattern).split(line)
+  else:
+    return None
+
+# update version info
 f = open('results.list')
 line = f.readline()
 line_num = 0
 while line:
-  if re.compile(", ").search(line):
-    divided = re.compile(", ").split(line)
+  divided = divide_line(line, ", ")
+  if divided is not None:
     uri=divided[0]
     version=divided[1]
-  
     line_num += update_version(uri, version)
   line = f.readline()
 f.close
 
-f = open('table.html', 'w')
-get_all_table = db.prepare("SELECT uri,status,version,updated,users,statuses,connections,registrations,ipv6,delay FROM list")
+# update delay info
+if path.exists('time.txt') :
+  f = open('time.txt')
+  line = f.readline()
+  line_num = 0
+  while line:
+    divided = divide_line(line, ", ")
+    if divided is not None:
+      uri = divided[0]
+      delay = float(divided[1])
+      line_num += update_delay(uri, delay)
+    line = f.readline()
+  f.close
 
+
+# write down table
+f = open('table.html', 'w')
+get_all_table = db.prepare("SELECT uri,status,version,updated,users,statuses,connections,registrations,ipv6,delay FROM list order by uri")
 f.write("<table id=\"listTable\" class=\"tablesorter\"><thead><tr><th>Instance</th><th>Status</th><th>Version</th><th>Updated</th><th>Users</th><th>Toots</th><th>Connections</th><th>Registrations</th><th>IPv6</th><th>Delay(s)</th></tr></thead><tbody>")
 
 with db.xact():
