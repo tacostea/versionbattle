c51863a56b1ec4674bce7ae94dc02e0b1dfbeecb
TacosTea <tacos@tacostea.net>
2017-07-19 18:44:57 +0900

    adapt result.txt format

--- dist.sh
+++ dist.sh
@@ -1,5 +1,1 @@
-if [ $# -eq 0 ]; then
-  echo "give me a parameter(file)!"
-  exit 1
-fi
-grep -E "[0-9]\.[0-9]" $1 | sort -k 2 -V | sed -r 's/^(.+), (.+\..+)/\2/' | uniq -c | sed -r 's/(.+) (.+)/[\2] \1/'
+grep -E "[0-9]\.[0-9]\.[0-9]" result.txt | sort -k 3 -V | sed -r 's/^(.+), ([0-9]\.[0-9]\.[0-9]), .*/\2/' | uniq -c | sed -r 's/(.+) (.+)/[\2] \1/'
